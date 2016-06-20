4058710958c89bf567f535df3df0bb494437f5f7
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2016-06-20 10:59:49 +0900

    Retouching some code

--- app/src/main/java/com/qwildz/digitallibrary/fragments/ListVideoFragment.java
+++ app/src/main/java/com/qwildz/digitallibrary/fragments/ListVideoFragment.java
@@ -3,6 +3,7 @@ package com.qwildz.digitallibrary.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.ColorUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
